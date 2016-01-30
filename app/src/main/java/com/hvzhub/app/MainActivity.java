fbefeca04d4ad138892bcaf2d84d72a985a4ab6a
Duncan Steenburgh <dst33nburgh@gmail.com>
2016-01-29 22:51:46 -0600

    Action bar now autohides on scroll down and reappears immediately on scroll up

--- app/src/main/java/com/hvzhub/app/MainActivity.java
+++ app/src/main/java/com/hvzhub/app/MainActivity.java
@@ -1,7 +1,8 @@
 package com.hvzhub.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
+
 }
