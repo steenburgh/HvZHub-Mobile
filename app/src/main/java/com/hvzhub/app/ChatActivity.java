59a2fda24b96fae758b39b0e30de1ee5c366e0e2
aedanflynn <lcolla@wisc.edu>
2016-02-03 12:41:10 -0600

    Added a Chat Activity and placeholder elements. Need fixing

---
+++ app/src/main/java/com/hvzhub/app/ChatActivity.java
@@ -0,0 +1,19 @@
+package com.hvzhub.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
