111de15a1d098da009ca28d0ac8e3313c49a3706
Duncan Steenburgh <dst33nburgh@gmail.com>
2016-03-14 23:02:06 -0500

    Fixed an issue with stay_logged_in not working  - The request looked like {"stayLoggedIn": true} instead of the proper format, {"stay_logged_in": true}

--- app/src/main/java/com/hvzhub/app/API/model/Login/LoginRequest.java
+++ app/src/main/java/com/hvzhub/app/API/model/Login/LoginRequest.java
@@ -1,11 +1,14 @@
 package com.hvzhub.app.API.model.Login;
 
+import com.google.gson.annotations.SerializedName;
+
 public class LoginRequest {
     private String email;
     private String password;
+
+    @SerializedName("stay_logged_in")
     private boolean stayLoggedIn;
 
-    public LoginRequest() {}
     public LoginRequest(String email, String password, boolean stayLoggedIn) {
         this.email = email;
         this.password = password;
