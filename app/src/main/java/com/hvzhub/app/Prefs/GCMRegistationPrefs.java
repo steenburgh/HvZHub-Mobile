25b87bd2ad914952fc977afdbf53549eee5240b2
Duncan Steenburgh <dst33nburgh@gmail.com>
2016-03-20 22:39:07 -0500

    [Refactoring] Renamed + Moved API preferences

--- app/src/main/java/com/hvzhub/app/Prefs/GCMRegistationPrefs.java
+++ app/src/main/java/com/hvzhub/app/Prefs/GCMRegistationPrefs.java
@@ -1,6 +1,6 @@
 package com.hvzhub.app.Prefs;
 
-public class QuickstartPreferences {
+public class GCMRegistationPrefs {
 
     public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     public static final String REGISTRATION_COMPLETE = "registrationComplete";
