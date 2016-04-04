1f22ebbae0b0a7a00c0dca85f51cb0a33d69bb27
Duncan Steenburgh <dst33nburgh@gmail.com>
2016-04-04 18:02:57 -0500

    Fixed Heatmap crash when no tags have a location yet.

--- app/src/main/java/com/hvzhub/app/HeatmapActivity.java
+++ app/src/main/java/com/hvzhub/app/HeatmapActivity.java
@@ -7,9 +7,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -90,6 +93,23 @@ public class HeatmapActivity extends FragmentActivity implements OnMapReadyCallb
                     }
 
                     addHeatMap(tagList);
+                    if (tagList.isEmpty()) {
+                        Snackbar sb = Snackbar.make(
+                                findViewById(R.id.map),
+                                R.string.no_tags_have_a_location_yet,
+                                Snackbar.LENGTH_INDEFINITE
+                        );
+                        sb.setAction(R.string.dismiss, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                // Do nothing. This should also auto-close the snackbar
+                            }
+                        });
+                        View view = sb.getView();
+                        TextView tv = (TextView) view.findViewById(android.support.design.R.id.snackbar_text);
+                        tv.setTextColor(Color.WHITE);
+                        sb.show();
+                    }
 
                 } else {
 
@@ -126,9 +146,9 @@ public class HeatmapActivity extends FragmentActivity implements OnMapReadyCallb
     }
 
     private void addHeatMap(List<LatLng> list) {
-
-//        ArrayList<LatLng> list = new ArrayList<>();
-//        list.add(new LatLng(43.075299, -89.40337299999999));
+        if (list == null || list.isEmpty()) {
+            return;
+        }
 
         int[] colors = {
                 Color.rgb(102, 225, 0), // green
@@ -148,7 +168,6 @@ public class HeatmapActivity extends FragmentActivity implements OnMapReadyCallb
         // Add a tile overlay to the map, using the heat map tile provider.
         mOverlay = heatMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
         mOverlay.clearTileCache();
-
     }
 
 
